bcae8b6a8d2af8b5a70c492ef096193b26ca76a6
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-01-02 18:10:21 +0530

    Create 11.cpp

---
+++ Milestone1/11.cpp
@@ -0,0 +1,64 @@
+class Solution{
+public:
+    // int *findTwoElement(int *arr, int n) 
+    // {
+        // // vector<int> v;
+        // int *ans=new int[2];
+        // sort(arr,arr+n);
+        // unordered_map<int,int>m;
+        
+        // for(int i=0;i<n;i++)
+        // {
+        //     m[arr[i]]++;
+        // }
+        
+        // for(auto x:m)
+        // {
+        //     if(x.second>1)
+        //     {
+        //         ans[0]=x.first;
+        //     }
+        // }
+        
+        // for(int i=1;i<=n;i++)
+        // {
+        //     if(m[i]==0)
+        //     {
+        //         ans[1]=i;
+        //         break;
+        //     }
+        // }
+        
+        // return ans;
+        // // code here
+    // }
+    
+    
+    int *findTwoElement(int *arr, int n)
+    {
+        int *ans=new int[2];
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[arr[i]]++;
+        }
+        
+        for(auto x:mp)
+        {
+            if(x.second>1)
+            {
+                ans[0]=x.first;
+            }
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            if(mp[i]==0)
+            {
+                ans[1]=i;
+                break;
+            }
+        }
+        return ans;
+    }
+};
